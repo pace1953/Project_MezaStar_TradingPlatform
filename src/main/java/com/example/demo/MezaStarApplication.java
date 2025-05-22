a65dad3c005e36a9cb4829f7f4fc8ec6c9db658a
enjoy <enjoy23519105@gmail.com>
2025-05-22 10:08:18 +0800

    2025/5/22 - First build, include basic settings.

---
+++ src/main/java/com/example/demo/MezaStarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MezaStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MezaStarApplication.class, args);
+	}
+
+}
